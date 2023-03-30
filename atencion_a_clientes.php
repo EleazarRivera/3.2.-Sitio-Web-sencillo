05171a67675c981bd95437521718a1ec3a09c86e
EleazarRivera <89939125+EleazarRivera@users.noreply.github.com>
2023-03-30 09:17:09 -0600

    Add files via upload

---
+++ atencion_a_clientes.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+
+    <header class="titulo1"> <h1> Atencion a clientes</h1></header>
+   
+<body class="fondo1">
+    <a class="active" href="index.php">Inicio</a></div>
+<nav>
+    <ul>
+        <li><a href="Acerca.php">Acerca de nosotros </a></li>
+    </ul>
+</nav>
+
+</div>
+
+<nav>
+    <ul>
+        <li><a href="ofertas.php"> ofertas</a></li>
+    </ul>
+</nav>
+</div>
+
+<nav>
+    <ul>
+        <li><a href="atencion_a_clientes.php">Atencion a clientes</a></li>
+    </ul>
+</nav>
+
+<div class="clearfix"></div>
+<h2 class="sub" > ¿Como podemos ayudarte?</h2><br><br>
+
+<h3 class="texto">A continuacion de presentamos un video informativo sobre la medicina neuropatica</h3>
+<div class="video">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/euJLF43LUpQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+ </div>
+ 
+ <h3>En otras noticias si te interesa :</h3>
+ <h2>Microsoft Adquirira a Activizion Blizzard</h2>
+ <audio controls>
+    <source src="audioNoticia.mp3" type="audio/mp3">
+    </audio>
+<div class="redesconte">
+
+<div class="redes">
+
+<img src="imagenes/wats2.jpg">
+<h2><a href="https://www.whatsapp.com/?lang=es"><center>222-212-333-44</center></a></h2>
+
+
+
+
+</div>
+<div class="redes">
+
+    <img src="imagenes/twiter.jpg">
+    <h2><a href="https://twitter.com/?lang=es">@lA YERBAMX</a></h2>
+    
+    
+    
+    
+    </div>
+    
+    <div class="redes">
+
+        <img src="imagenes/facebook.jpg">
+        <h2><a href="https://es-la.facebook.com/"><center>LA YERBA</center></a></h2>
+        
+        
+        
+        
+        </div>
+        <div class="redes">
+
+            <img src="imagenes/insta.jpg">
+            <h2><a href="https://www.instagram.com/?hl=es"><center>@LA YERBA</center></a></h2>
+            
+            
+            
+            
+            </div>
+            
+            
+        
+            
+
+
+ </div>   
+ <div class="clearfix"></div>
+
+<footer>
+
+<p>Tienda Naturistas "LA YERBA" &COPY 2022</p>
+<address>
+<a href="mailto: Riveraeleazarmec@gmail.com"> Riveraeleazarmec@gmail.com</a>
+</address>
+</footer>
+</body>
+</html>
